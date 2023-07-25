cd239f367c33fc4c4e8f86ae5693f837c7eda65d
Christian Soltenborn <christian.soltenborn@fhdw.de>
2023-07-25 14:08:40 +0200

    button activated

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.Date;
 
 public class MainFragment extends Fragment {
 
@@ -18,4 +24,20 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getButton().setOnClickListener(vi -> getTextView().setText("Button clicked on " + new Date()));
+    }
+
+    private TextView getTextView() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.textView);
+    }
+
+    private Button getButton() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.button);
+    }
 }
