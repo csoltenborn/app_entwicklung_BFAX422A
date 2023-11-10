f4efbb11c444325df66f7c821df07b18464c09d8
AnMiGa <angela.gaugler@gmx.de>
2023-11-10 11:31:20 +0100

    created simple widget

---
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/WidgetProvider.java
@@ -0,0 +1,23 @@
+package de.fhdw.app_entwicklung.chatgpt;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for(int appWidgetId: appWidgetIds){
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
