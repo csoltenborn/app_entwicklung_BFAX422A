f92d11827883ccd72f005225f8d29d1dc2587639
Finn Ehrlich <finn.ehrlich@dittrich-kollegen.de>
2023-11-11 14:18:42 +0100

    Design gebaut und wechsel funktionalität zwischen chat und quiz

---
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/QuizFragment.java
@@ -0,0 +1,73 @@
+package de.fhdw.app_entwicklung.chatgpt;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuizFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuizFragment extends Fragment {
+
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public QuizFragment() {
+
+    }
+
+
+    public static QuizFragment newInstance(String param1, String param2) {
+        QuizFragment fragment = new QuizFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Intent i = new Intent(this.getContext(), MainActivity.class);
+        getChatButton().setOnClickListener(v-> startActivity(i));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    private Button getChatButton()
+    {
+        return getView().findViewById(R.id.toChat);
+    }
+}
