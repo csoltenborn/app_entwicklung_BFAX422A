603d30438a5772f5ba876c3b35a53dba32356116
A1B8268 <tanja.vetter@rewe-group.com>
2023-11-11 14:48:28 +0100

    text and speech input

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -1,10 +1,12 @@
 package de.fhdw.app_entwicklung.chatgpt;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
@@ -12,13 +14,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Locale;
 
 import de.fhdw.app_entwicklung.chatgpt.model.Author;
 import de.fhdw.app_entwicklung.chatgpt.model.Chat;
+import de.fhdw.app_entwicklung.chatgpt.model.ChatAdapter;
 import de.fhdw.app_entwicklung.chatgpt.model.Message;
 import de.fhdw.app_entwicklung.chatgpt.openai.ChatGpt;
-import de.fhdw.app_entwicklung.chatgpt.model.ChatAdapter;
 import de.fhdw.app_entwicklung.chatgpt.speech.LaunchSpeechRecognition;
 import de.fhdw.app_entwicklung.chatgpt.speech.TextToSpeechTool;
 
@@ -30,31 +34,14 @@ public class MainFragment extends Fragment {
     private TextToSpeechTool textToSpeech;
     private Chat chat;
     private ChatAdapter chatAdapter;
+    private RecyclerView recyclerView;
 
     private final ActivityResultLauncher<LaunchSpeechRecognition.SpeechRecognitionArgs> getTextFromSpeech = registerForActivityResult(
             new LaunchSpeechRecognition(),
-            query -> {
-                Message userMessage = new Message(Author.User, query);
-                chat.addMessage(userMessage);
-                chatAdapter.notifyItemInserted(chat.getMessages().size() - 1);
-
-                MainActivity.backgroundExecutorService.execute(() -> {
-                    String apiToken = prefs.getApiToken();
-                    ChatGpt chatGpt = new ChatGpt(apiToken);
-                    String answer = chatGpt.getChatCompletion(chat);
-
-                    Message answerMessage = new Message(Author.Assistant, answer);
-                    chat.addMessage(answerMessage);
-                    textToSpeech.speak(answer);
-                    requireActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            chatAdapter.notifyItemInserted(chat.getMessages().size() - 1);
-                        }
-                    });
-                });
-            }
+            this::askChatGPT
     );
+
+
 
     public MainFragment() {
     }
@@ -77,12 +64,18 @@ public class MainFragment extends Fragment {
             chat = savedInstanceState.getParcelable(EXTRA_DATA_CHAT);
         }
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView = view.findViewById(R.id.recyclerView);
         chatAdapter = new ChatAdapter(chat.getMessages(), requireContext());
         recyclerView.setAdapter(chatAdapter);
 
-        getAskButton().setOnClickListener(v ->
-                getTextFromSpeech.launch(new LaunchSpeechRecognition.SpeechRecognitionArgs(Locale.GERMAN)));
+        getMicButton().setOnClickListener(v -> getTextFromSpeech.launch(new LaunchSpeechRecognition.SpeechRecognitionArgs(Locale.GERMAN)));
+
+        getSendButton().setOnClickListener(v -> {
+            askChatGPT(getQuestion().getText().toString());
+            hideKeyboard();
+            getQuestion().setText("");
+            recyclerView.scrollToPosition(chatAdapter.getItemCount() - 1);
+        });
     }
 
     @Override
@@ -104,9 +97,46 @@ public class MainFragment extends Fragment {
         super.onDestroy();
     }
 
-    private Button getAskButton() {
-        //noinspection ConstantConditions
-        return getView().findViewById(R.id.button_ask);
+    public void askChatGPT(String query) {
+        Message userMessage = new Message(Author.User, query);
+        chat.addMessage(userMessage);
+        chatAdapter.notifyItemInserted(chat.getMessages().size() - 1);
+
+        MainActivity.backgroundExecutorService.execute(() -> {
+            String apiToken = prefs.getApiToken();
+            ChatGpt chatGpt = new ChatGpt(apiToken);
+            String answer = chatGpt.getChatCompletion(chat);
+
+            Message answerMessage = new Message(Author.Assistant, answer);
+            chat.addMessage(answerMessage);
+            textToSpeech.speak(answer);
+            requireActivity().runOnUiThread(() -> {
+                chatAdapter.notifyItemInserted(chat.getMessages().size() - 1);
+                recyclerView.scrollToPosition(chatAdapter.getItemCount() - 1);
+            });
+        });
     }
 
+    private void hideKeyboard() {
+        View view = requireActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private FloatingActionButton getMicButton() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.button_mic);
+    }
+
+    private FloatingActionButton getSendButton() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.button_send);
+    }
+
+    private EditText getQuestion() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.question);
+    }
 }
